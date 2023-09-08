34b0dad7db47abfe7b3b2303e800fad497fc8555
iftekharzeeon <1805038@ugrad.cse.buet.ac.bd>
2023-09-09 02:45:45 +0600

    Payment success updated

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -11,7 +11,7 @@ const mainUrl = process.env.MAINURL
 
 //sslcommerz init
 const paymentInit = async (req, res) => {
-    const { busScheduleId, userId, passengerInfo, totalFare, numberOfTickets, ticketId } = req.body;
+    const { ticketInfo, userId, grandTotalFare } = req.body;
 
     // Generate unique transaction ID of 20 characters length mixed with letters and numbers
     const transactionId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
@@ -25,12 +25,18 @@ const paymentInit = async (req, res) => {
 
     // Get current date and time
     const dateTime = date + ' ' + time;
+    let ticketsIds = "";
+    let busScheduleIds = "";
+    for (let i = 0; i < ticketInfo.length; i++) {
+        ticketsIds += ticketInfo[i].ticketId + '-';
+        busScheduleIds += ticketInfo[i].busScheduleId + '-';
+    }
 
     const data = {
-        total_amount: totalFare,
+        total_amount: grandTotalFare,
         currency: 'BDT',
         tran_id: transactionId, // use unique tran_id for each api call
-        success_url: `${mainUrl}/paymentSuccess\\${busScheduleId}\\${ticketId}`,
+        success_url: `${mainUrl}/paymentSuccess\\${busScheduleIds}\\${ticketsIds}`,
         fail_url: `${mainUrl}/paymentFail`,
         cancel_url: `${mainUrl}/cancel`,
         ipn_url: `${mainUrl}/paymentIpn`,
@@ -61,23 +67,30 @@ const paymentInit = async (req, res) => {
         // Redirect the user to payment gateway
 
         //save transaction info to database
-        const insertIntoTicketInfoQuery = {
-            text: `INSERT INTO ticket_info (ticket_id, user_id, bus_schedule_id, 
-                number_of_tickets, total_fare, passenger_info, transaction_id, date) 
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-            values: [ticketId, userId, busScheduleId, numberOfTickets, totalFare, passengerInfo, transactionId, dateTime]
-        }
-        busPool.query(insertIntoTicketInfoQuery, (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    status: 'fail',
-                    message: 'Database error',
-                    data: err
-                });
+        for (let i = 0; i < ticketInfo.length; i++) {
+            const ticketId = ticketInfo[i].ticketId;
+            const busScheduleId = ticketInfo[i].busScheduleId;
+            const numberOfTickets = ticketInfo[i].numberOfTickets;
+            const totalFare = ticketInfo[i].totalFare;
+            const passengerInfo = ticketInfo[i].passengerInfo;
+            const insertIntoTicketInfoQuery = {
+                text: `INSERT INTO ticket_info (ticket_id, user_id, bus_schedule_id, 
+                    number_of_tickets, total_fare, passenger_info, transaction_id, date) 
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+                values: [ticketId, userId, busScheduleId, numberOfTickets, totalFare, passengerInfo, transactionId, dateTime]
             }
-            console.log('Ticket info saved to database');
-        });
+            busPool.query(insertIntoTicketInfoQuery, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        status: 'fail',
+                        message: 'Database error',
+                        data: err
+                    });
+                }
+                console.log('Ticket info saved to database');
+            });
+        }
         
         let GatewayPageURL = apiResponse.GatewayPageURL
         console.log('Redirecting to: ', apiResponse)
@@ -91,8 +104,8 @@ const paymentSuccess = async (req, res) => {
     // Get ticket id and bus schedule id from url using split
     const url = req.url;
     const urlArray = url.split('\\');
-    const busScheduleId = urlArray[1];
-    const ticketId = urlArray[2];
+    const busScheduleIds = urlArray[1];
+    const ticketIds = urlArray[2];
 
     const data = req.body;
     const ssl = new SSLCommerzPayment(store_id, store_passwd, is_live)
@@ -106,29 +119,47 @@ const paymentSuccess = async (req, res) => {
 
     const transactionId = data.tran_id;
     const paymentMedium = data.card_issuer;
-    const updateTicketInfoQuery = {
-        text: `UPDATE ticket_info SET payment_medium = $1, payment_status = $2 WHERE transaction_id = $3`,
-        values: [paymentMedium, 1, transactionId]
-    }
-    busPool.query(updateTicketInfoQuery, (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({
-                status: 'fail',
-                message: 'Database error',
-                data: err
-            });
-        }
-        console.log('Ticket info updated to database');
-    });
+    const busScheduleIdArray = busScheduleIds.split('-');
+    const ticketIdArray = ticketIds.split('-');
+    for (let i = 0; i < ticketIdArray.length - 1; i++) {
+        const ticketId = ticketIdArray[i];
+        const busScheduleId = busScheduleIdArray[i];
 
-    // Update bus schedule info
-    const updateBusScheduleQuery = {
-        text: `UPDATE bus_schedule_seat_info 
-        SET booked_status = 2 
-        WHERE bus_schedule_id = $1 
-        AND ticket_id = $2`,
-        values: [busScheduleId, ticketId]
+        const updateTicketInfoQuery = {
+            text: `UPDATE ticket_info SET payment_medium = $1, payment_status = $2 WHERE transaction_id = $3`,
+            values: [paymentMedium, 1, transactionId]
+        }
+        busPool.query(updateTicketInfoQuery, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    status: 'fail',
+                    message: 'Database error',
+                    data: err
+                });
+            }
+            console.log('Ticket info updated to database');
+        });
+
+        // Update bus schedule info
+        const updateBusScheduleQuery = {
+            text: `UPDATE bus_schedule_seat_info 
+            SET booked_status = 2 
+            WHERE bus_schedule_id = $1 
+            AND ticket_id = $2`,
+            values: [busScheduleId, ticketId]
+        }
+        busPool.query(updateBusScheduleQuery, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    status: 'fail',
+                    message: 'Database error',
+                    data: err
+                });
+            }
+            console.log('Bus schedule info updated to database');
+        });
     }
     return res.status(200).json({
         status: 'success',
